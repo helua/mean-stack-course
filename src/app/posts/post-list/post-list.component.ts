9998d265a5cf7917ddcf0798d997304608779c92
helua <helua@users.noreply.github.com>
2021-04-13 14:43:09 +0200

    first commit

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../post.model';
+
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  @Input() posts: Post[] = [];
+  ngOnInit(): void{
+    console.log(this.posts);
+  }
+}
